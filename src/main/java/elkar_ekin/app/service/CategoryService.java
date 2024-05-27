10e89cb854f427bf435ce50fee952988b1e84278
Aritz Estevez <ariestevev@gmail.com>
2024-05-27 16:45:36 +0200

    con error doble categoryID

---
+++ src/main/java/elkar_ekin/app/service/CategoryService.java
@@ -0,0 +1,10 @@
+package elkar_ekin.app.service;
+
+import elkar_ekin.app.dto.CategoryDto;
+import elkar_ekin.app.model.Category;
+
+public interface CategoryService {
+    
+
+    public Category save (CategoryDto categoryDto);
+}
