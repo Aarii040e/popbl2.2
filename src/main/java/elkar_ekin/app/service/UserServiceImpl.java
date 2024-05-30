609d1a36f41431bd9033aae6c53a559587b417bd
Amaia Bartolomé <amaiabart@gmail.com>
2024-05-30 16:32:58 +0200

    cambiar pfp desde página del usuario

--- src/main/java/elkar_ekin/app/service/UserServiceImpl.java
+++ src/main/java/elkar_ekin/app/service/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package elkar_ekin.app.service;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import elkar_ekin.app.dto.UserDto;
 import elkar_ekin.app.model.User;
@@ -33,7 +40,26 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User update(UserDto userDto, UserDetails userDetails) {
 		User user = userRepository.findByUsername(userDetails.getUsername());
+
+		MultipartFile image = userDto.getImageFile();
+        try {
+			if (image != null) {
+				String uploadDir = "public/images/";
+				Path uploadPath = Paths.get(uploadDir);
+				if(!Files.exists(uploadPath)){
+					Files.createDirectories(uploadPath);
+				}
+				try (InputStream inputStream = image.getInputStream()){
+					Files.copy(inputStream, Paths.get(uploadDir + image.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+				}
+			}
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
 		user.setDescription(userDto.getDescription());
+		System.out.println(userDto.getImageFile().getOriginalFilename());
+		if (userDto.getImageFile().getOriginalFilename() != "") user.setImagePath(userDto.getImageFile().getOriginalFilename());
 		return userRepository.save(user);
 	}
 }
