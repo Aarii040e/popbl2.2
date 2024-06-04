8d486a20c38ea106c4a42637aef738bc6d7d726d
Aritz Estevez <ariestevev@gmail.com>
2024-06-04 15:11:17 +0200

    Al voluntario solo le salen las tasks que estan activas y cuya fecha no sea anterior a la actual

--- src/main/java/elkar_ekin/app/service/TaskServiceImpl.java
+++ src/main/java/elkar_ekin/app/service/TaskServiceImpl.java
@@ -109,11 +109,18 @@ public class TaskServiceImpl implements TaskService {
 		existingTask.setEndTime(taskDto.getEndTime());
 		taskRepository.save(existingTask);
     }
+	
 	@Override
 	public List<Task> getAllActiveTasks() {
         List<Task> tasks = taskRepository.findTasksByState("active");
+		LocalDateTime currentDate = LocalDateTime.now();
         // Si necesitas transformar los datos de alguna manera, puedes hacerlo aquí
-        return tasks.stream().map(item -> {
+        return tasks.stream().filter(item -> {
+			LocalDate date = item.getDate();
+				LocalTime endTime = item.getEndTime();
+				LocalDateTime taskDateTime = LocalDateTime.of(date, endTime);
+				return taskDateTime.isAfter(currentDate);
+		}).map(item -> {
             Task task = new Task();
 			task.setTaskID(item.getTaskID());
             task.setDescription(item.getDescription());
@@ -126,7 +133,7 @@ public class TaskServiceImpl implements TaskService {
 			return task;
         }).collect(Collectors.toList());
 	}
-
+	
 	public List<Task> getAllPastTasks(User volunteer) {
         List<Task> tasks = taskRepository.findTasksByClient(volunteer);
 		LocalDateTime currentDate = LocalDateTime.now();
