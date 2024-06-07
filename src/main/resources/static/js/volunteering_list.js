271646a2c197d31fbf7592df4ddef6dea83a2939
Amaia Bartolomé <amaiabart@gmail.com>
2024-06-07 16:42:44 +0200

    bookmark bien

--- src/main/resources/static/js/volunteering_list.js
+++ src/main/resources/static/js/volunteering_list.js
@@ -2,17 +2,21 @@
 
 cut(".description", 120);
 
+// Código bookmark
+
 document.querySelectorAll(".bookmark").forEach(bookmark => {
     let currentBookmark = bookmark.getAttribute('id');
     let list = document.querySelector(".taskIDList").innerText.replace(/[\[\]]/g,'').replace(/\s/g,'').split(",");
-    if(list.includes(currentBookmark)){
+    if (list.includes(currentBookmark)) {
         bookmark.classList.add("fa-solid");
-        bookmark.classList.remove("fa-regular")
+        bookmark.classList.remove("fa-regular");
         bookmark.href="/volunteer-view/task/" + bookmark.getAttribute('id') + "/delete";
     }
-    else{
-        bookmark.classList.remove("fa-solid")
-        bookmark.classList.add("fa-regular")
+    else {
+        bookmark.classList.remove("fa-solid");
+        bookmark.classList.add("fa-regular");
         bookmark.href="/volunteer-view/task/" + bookmark.getAttribute('id') + "/save";
     }
+
+    document.querySelectorAll(".signup").forEach(button => bookmark.style.right = `${button.clientWidth + 18}px`);
 });
